63a898b8aaabea8c655417d231151acf92e82cad
Eliza Mutu <elizamutu@yahoo.com>
2021-10-25 15:59:37 +0300

    url

--- forum/app/core/app.php
+++ forum/app/core/app.php
@@ -5,14 +5,33 @@ class App {
     private $controller = "home";
     private $method  = "index";
     private $params = [];
+
     public function __construct() {
 
         $url = $this->splitURL();
-        show($url);
+        
+        if(file_exists("../app/controllers/". strtolower($url[0]) .".php")){
+            $this->controller = strtolower($url[0]);
+            unset($url[0]);
+        }
+
+        require "../app/controllers/". $this->controller .".php";
+        $this->controller = new $this->controller;
+
+        if(isset($url[1])) {
+            if(method_exists($this->controller, $url[1])) {
+                
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+        //run the class and method
+        $this->params = array_values($url);
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
     private function splitURL() {
-
-        return explode("/", trim($_GET['url'], "/"));
+        $url = isset($_GET['url']) ? $_GET['url'] : "home";//if the $_GET['url'] is empty, assign it to home
+        return explode("/", filter_var(trim($url, "/"),FILTER_SANITIZE_URL));
     }
 }
