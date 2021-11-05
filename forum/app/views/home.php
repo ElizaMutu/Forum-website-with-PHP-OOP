ef41d5984981d24942eb17d7c0029c7e6134f4ca
Eliza Mutu <elizamutu@yahoo.com>
2021-11-05 23:34:08 +0200

    navbar

--- forum/app/views/home.php
+++ forum/app/views/home.php
@@ -1,7 +1,28 @@
+
 <?php $this->view("header"); ?>
 
+<header>
+    <div class="navbar">
+        <ul class="navbar-nav">
+            <li>
+                <a href="#" class="logo">Discussion Forum</a>
+            </li>
+        
+            <li class="dropdown">
+                <div class="dropbtn">My Account</div>
+                <ul class="dropdown-content">
+                    <li><a href="#">Login</a></li>
+                    <li><a href="#">Register</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</header>
+
+
 <body>
-    
+    <img src="<?=ASSETS?>/css/fantasy-g.jpg" alt="Bridge">
+
 
 </body>
 
